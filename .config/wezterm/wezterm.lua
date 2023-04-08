d4ccf8d6111842b8705c36127432979e67b90304
Yomi Ikuru <captyomjnr@gmail.com>
2023-04-08 23:04:51 +0100

    Add Wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,36 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'AdventureTime'
+-- config.color_scheme = 'Batman'
+
+config.use_fancy_tab_bar = false
+
+wezterm.on('update-right-status', function(window, pane)
+  -- "Wed Mar 3 08:14"
+  local date = wezterm.strftime '%a %b %-d %H:%M '
+
+  local bat = ''
+  for _, b in ipairs(wezterm.battery_info()) do
+    bat = '🔋 ' .. string.format('%.0f%%', b.state_of_charge * 100)
+  end
+
+  window:set_right_status(wezterm.format {
+    { Text = bat .. '   ' .. date },
+  })
+end)
+
+-- and finally, return the configuration to wezterm
+return config
